52f9d5627189a820ae157b216f46e356b54548c6
Chris Gregori <chris.gregori@whitehat.org.uk>
2019-11-01 17:25:18 +0000

    Animating cards

---
+++ TutorialApp/ContentView.swift
@@ -0,0 +1,143 @@
+//
+//  ContentView.swift
+//  TutorialApp
+//
+//  Created by Chris Gregori on 01/11/2019.
+//  Copyright © 2019 Chris Gregori. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var show = false
+    
+    var body: some View {
+        ZStack {
+            TitleView()
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
+            
+            CardBottomView()
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
+            
+            CardView()
+                .background(show ? Color.green : Color("background9"))
+                .cornerRadius(10)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -400 : -40 )
+                .scaleEffect(0.85)
+                .rotationEffect(Angle(degrees: show ? 15 : 0))
+                .rotation3DEffect(Angle(degrees: show ? 50 : 0), axis:
+                    (x: 10.0, y: 10.0, z: 10.0))
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.7))
+            
+            CardView()
+                .background(show ? Color.red : Color("background8"))
+                .cornerRadius(10)
+                .shadow(radius: 20)
+                .offset(x: 0 , y: show ? -200 : -20)
+                .scaleEffect(0.9)
+                .rotationEffect(Angle(degrees: show ? 10 : 0))
+                .rotation3DEffect(Angle(degrees: show ? 40 : 0), axis:
+                    (x: 10.0, y: 10.0, z: 10.0))
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.5))
+            
+            CertificateView()
+                .scaleEffect(0.95)
+                .rotationEffect(Angle(degrees: show ? 5 : 0))
+                .rotation3DEffect(Angle(degrees: show ? 30 : 0), axis:
+                    (x: 10.0, y: 10.0, z: 10.0))
+                .animation(.spring())
+                .onTapGesture {
+                    self.show.toggle()
+                }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            Text("Card Back")
+            
+        }
+        .frame(width: 340.0, height: 220.0)
+    }
+}
+
+struct CertificateView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("UI Design!")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("accent"))
+                        .padding(.top)
+                    Text("Certificate")
+                        .foregroundColor(Color.white)
+                }
+                Spacer()
+                Image("Logo")
+                    .resizable()
+                    .frame(width: 30.0, height: 30.0)
+            }
+            .padding(.horizontal)
+            Spacer()
+            Image("Background")
+        }
+        .frame(width: 340.0, height: 220.0)
+        .background(Color.black)
+        .cornerRadius(10)
+        .shadow(radius: 20)
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Certificates")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                Spacer()
+            }
+            Image("Illustration5")
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct CardBottomView: View {
+    var body: some View {
+        VStack(spacing: 20.0) {
+            Rectangle()
+                .frame(width: 60, height: 6)
+                .cornerRadius(3)
+                .opacity(0.1)
+            
+            Text("This is just some text that's going to appear here in a card. Bla blah this is some lorem upsum stuff")
+                .lineLimit(10)
+            Spacer()
+        }
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .padding()
+        .padding(.horizontal)
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(radius: 20)
+        .offset(y: 600.0)
+    }
+}
